435ffbbfd0f96833d270200908864bc1f3c7ab54
Ivan <39248996+kLubidorka@users.noreply.github.com>
2021-01-22 23:33:35 +0400

    refactor

--- ConnectionToDb.py
+++ ConnectionToDb.py
@@ -4,8 +4,10 @@ from pymysql.cursors import DictCursor
 tablename = "AIRTRANS"
 
 
-def execute_statement(cursor, stmt):
-    cursor.execute(stmt)
+def execute_statement(connection, stmt, commit=False):
+    connection.cursor().execute(stmt)
+    if commit:
+        connection.commit()
 
 
 def connect_to_airtrans_db():
@@ -17,23 +19,23 @@ def connect_to_airtrans_db():
             charset='utf8mb4',
             cursorclass=DictCursor
         )
-        with connection.cursor() as cursor:
-            execute_statement(cursor, f"CREATE DATABASE IF NOT EXISTS {tablename}")
-            execute_statement(cursor, f"USE {tablename}")
-            with open('INIT_DB.sql', mode='r') as initial_sql_script:
-                sqlStatements = initial_sql_script.read().split(';')
-                for statement in sqlStatements:
-                    if len(str.strip(statement)) != 0:
-                        execute_statement(cursor, statement)
-                print("database is created and filled")
+        execute_statement(connection, f"CREATE DATABASE IF NOT EXISTS {tablename}")
+        execute_statement(connection, f"USE {tablename}")
+        with open('INIT_DB.sql', mode='r') as initial_sql_script:
+            sqlStatements = initial_sql_script.read().split(';')
+            for statement in sqlStatements:
+                if len(str.strip(statement)) != 0:
+                    execute_statement(connection, statement)
+            connection.commit()
+            print("database is created and filled")
+
     except Exception as e:
         print("Exeception occured:{}".format(e))
     return connection
 
 
 def release_resources(connection):
-    with connection.cursor() as cursor:
-        execute_statement(cursor, f"DROP DATABASE {tablename}")
+    execute_statement(connection, f"DROP DATABASE {tablename}", commit=True)
     connection.close()
 
 
