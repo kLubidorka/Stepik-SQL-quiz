dd5350ca0dc4e33c6cb4f46bfea35d5f1ab2839a
Ivan <39248996+kLubidorka@users.noreply.github.com>
2021-01-22 23:29:24 +0400

    refactor

--- ConnectionToDb.py
+++ ConnectionToDb.py
@@ -1,6 +1,8 @@
 import pymysql
 from pymysql.cursors import DictCursor
 
+tablename = "AIRTRANS"
+
 
 def execute_statement(cursor, stmt):
     cursor.execute(stmt)
@@ -15,25 +17,29 @@ def connect_to_airtrans_db():
             charset='utf8mb4',
             cursorclass=DictCursor
         )
-        cursor = connection.cursor()
-        execute_statement(cursor, "CREATE DATABASE IF NOT EXISTS AIRTRANS")
-        execute_statement(cursor, "USE AIRTRANS")
-        with open('INIT_DB.sql', mode='r') as initial_sql_script:
-            sqlStatements = initial_sql_script.read().split(';')
-            for statement in sqlStatements:
-                if len(str.strip(statement)) != 0:
-                    execute_statement(cursor, statement)
-            print("database is created and filled")
-        return connection
+        with connection.cursor() as cursor:
+            execute_statement(cursor, f"CREATE DATABASE IF NOT EXISTS {tablename}")
+            execute_statement(cursor, f"USE {tablename}")
+            with open('INIT_DB.sql', mode='r') as initial_sql_script:
+                sqlStatements = initial_sql_script.read().split(';')
+                for statement in sqlStatements:
+                    if len(str.strip(statement)) != 0:
+                        execute_statement(cursor, statement)
+                print("database is created and filled")
     except Exception as e:
         print("Exeception occured:{}".format(e))
-        execute_statement(cursor, "DROP DATABASE AIRTRANS")
-        connection.close()
+    return connection
+
+
+def release_resources(connection):
+    with connection.cursor() as cursor:
+        execute_statement(cursor, f"DROP DATABASE {tablename}")
+    connection.close()
 
 
 def main():
     connection = connect_to_airtrans_db()
-    connection.close()
+    release_resources(connection)
 
 
 if __name__ == '__main__':
